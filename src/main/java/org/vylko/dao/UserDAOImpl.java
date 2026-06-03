6dc2f4a9954a94491da1f67aa93d41d0f1bb88d9
Evgeny Vylko <belfoot10@gmail.com>
2026-06-03 15:09:34 +0300

    create dao impl class

---
+++ src/main/java/org/vylko/dao/UserDAOImpl.java
@@ -0,0 +1,42 @@
+package org.vylko.dao;
+
+import org.springframework.stereotype.Repository;
+import org.vylko.entity.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public User findById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void update(User user) {
+        User userToUpdate = entityManager.find(User.class, user.getId());
+        userToUpdate.setName(user.getName());
+        userToUpdate.setEmail(user.getEmail());
+    }
+
+    @Override
+    public void delete(Long id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return entityManager.createQuery("From User", User.class).getResultList();
+    }
+}
