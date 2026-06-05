63a4b699bd4c18c463c861dc76af21e781d962ed
Evgeny Vylko <belfoot10@gmail.com>
2026-06-05 13:49:55 +0300

    add RequestMapping to controller

--- src/main/java/org/vylko/controller/UserController.java
+++ src/main/java/org/vylko/controller/UserController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.vylko.entity.User;
 import org.vylko.service.UserService;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -17,11 +21,22 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public String index(Model model) {
         List<User> userList = userService.findAll();
         model.addAttribute("userList", userList);
         return "index";
     }
 
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "newUser";
+    }
+
+    @PostMapping("/create")
+    public String createNewUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
 }
