6f4b7b6993a464eba9a9e9e57ff7e50e4734bd61
Evgeny Vylko <belfoot10@gmail.com>
2026-06-05 11:48:09 +0300

    create index method

--- src/main/java/org/vylko/controller/UserController.java
+++ src/main/java/org/vylko/controller/UserController.java
@@ -1,4 +1,27 @@
 package org.vylko.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.vylko.entity.User;
+import org.vylko.service.UserService;
+import java.util.List;
+
+@Controller
 public class UserController {
+
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String index(Model model) {
+        List<User> userList = userService.findAll();
+        model.addAttribute("userList", userList);
+        return "index";
+    }
+
 }
